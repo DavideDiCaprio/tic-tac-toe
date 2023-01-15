adf8ed79d75e750219678eeb946438963ec81d6b
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-01-15 16:46:34 +0100

    add print_board function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -73,3 +73,14 @@ def get_winner(board):
 
   else :
     return None
+
+  
+def print_board(board, n_spaces_offset=25):
+  offset = ' '*n_spaces_offset
+  print()
+  print(offset+f' {board[0][0]} | {board[0][1]} | {board[0][2]}')
+  print(offset+'_'*11)
+  print(offset+f' {board[1][0]} | {board[1][1]} | {board[1][2]}')
+  print(offset+'_'*11)
+  print(offset+f' {board[2][0]} | {board[2][1]} | {board[2][2]}')
+  print()
