cca21f812dd27c6912809ea8f9a327997e2dbbf7
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-01-30 18:27:01 +0100

    Update tic-tac-toe_utils.py

--- tic-tac-toe_utils.py
+++ tic-tac-toe_utils.py
@@ -1,2 +1,11 @@
 import random
 from copy import deepcopy
+
+
+def is_any_move_possible(board) -> bool:
+  
+  for i in range(len(board)):
+    if ' ' in board[i]:
+      return True
+
+  return False
