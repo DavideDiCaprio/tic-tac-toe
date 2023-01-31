966309585226c8c53e1608e4c762bb07a3b73c03
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-01-31 21:19:48 +0100

    add function

--- tic-tac-toe_utils.py
+++ tic-tac-toe_utils.py
@@ -9,3 +9,90 @@ def is_any_move_possible(board) -> bool:
       return True
 
   return False
+
+
+def print_board(board, n_spaces_offset=25):
+  offset = ' '*n_spaces_offset
+  print()
+  print(offset+f' {board[0][0]} | {board[0][1]} | {board[0][2]}')
+  print(offset+'_'*11)
+  print(offset+f' {board[1][0]} | {board[1][1]} | {board[1][2]}')
+  print(offset+'_'*11)
+  print(offset+f' {board[2][0]} | {board[2][1]} | {board[2][2]}')
+  print()
+
+
+def get_winner(board):
+  
+  if check_horizontal_win(board) == "X" or check_horizontal_win(board) == "O":
+    return check_horizontal_win(board)
+
+  elif check_diagonal_win(board) == "X" or check_diagonal_win(board) == "O":
+    return check_diagonal_win(board)
+
+  elif check_vertical_win(board) == "X" or check_vertical_win(board) == "O":
+    return check_vertical_win(board)
+    
+  else :
+    return None
+
+
+def check_horizontal_win(board):
+
+  for i in range(len(board)):
+    if board[i] == ["O","O","O"]:
+      return "O"
+    elif board[i] == ["X","X","X"]:
+      return "X"
+      
+  return None 
+
+
+def check_vertical_win(board):
+  
+  if board[0][0] == "X" and board[1][0]== "X" and board[2][0] =="X":
+    return "X"
+      
+  elif board[0][1] == "X" and board[1][1]== "X" and board[2][1] =="X":
+    return "X"
+    
+  elif board[0][2] == "X" and board[1][2]== "X" and board[2][2] =="X":
+    return "X"
+
+  elif board[0][0] == "O" and board[1][0]== "O" and board[2][0] =="O":
+    return "O"
+      
+  elif board[0][1] == "O" and board[1][1]== "O" and board[2][1] =="O":
+    return "O"
+    
+  elif board[0][2] == "O" and board[1][2]== "O" and board[2][2] =="O":
+    return "O"
+
+  return None
+
+
+def check_diagonal_win(board):
+  
+  if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+    return "X"
+    
+  elif board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+    return "X"
+
+  elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+    return "O"
+    
+  elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+    return "O"
+
+  return None
+
+
+def computer_move(board):
+  
+  while True:
+    x = random.randint(0,2)
+    y = random.randint(0,2)
+
+    if board[x][y] == " ":
+      return x,y
